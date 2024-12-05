cbec4a94060ff2648f6ed3803b3de39eca1679bf
Christian Leingang <cleingang@pm.me>
2024-12-05 16:22:09 +0100

    switch to deno

---
+++ utils/run.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'https://deno.land/std@0.224.0/path/mod.ts';
+
+export function getInput(dayPath: string, isTest: boolean): string {
+  const fileName = isTest ? 'input_test.txt' : 'input.txt';
+  return Deno.readTextFileSync(resolve(dayPath, fileName));
+}
+
+const isTest: boolean = Deno.args[0] === 'test';
+const parameter: number = isTest ? 3 : 2;
+const day: string = Deno.args[parameter] || new Date().getDate().toString().padStart(2, '0');
+const input: string = getInput(`./day${day}/`, isTest);
+
+import(`../day${day}/index.ts`).then((module) => {
+  module.default(input);
+});
--- utils/setup.ts
+++ utils/setup.ts
@@ -1,37 +1,37 @@
-import fs from 'fs';
+import { existsSync } from 'https://deno.land/std@0.224.0/fs/exists.ts';
 
 // Get the current day of the month
 const day = new Date().getDate().toString().padStart(2, '0');
 
 // Create the directory
 const dir = `./day${day}`;
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir);
+if (!existsSync(dir)) {
+  Deno.mkdirSync(dir);
 }
 
 // Create the input files
-fs.writeFileSync(`${dir}/input.txt`, '');
-fs.writeFileSync(`${dir}/input_test.txt`, '');
+await Deno.writeTextFileSync(`${dir}/input.txt`, '');
+await Deno.writeTextFileSync(`${dir}/input_test.txt`, '');
 
-// Create the index.js file
+// Create the index.ts file
 const code = `
-function part1(input) {
+function part1(input: string[]) {
   let result = 0;
   console.log('Part 1: ', result);
 }
 
-function part2(input) {
+function part2(input: string[]) {
   let result = 0;
   console.log('Part 2: ', result);
 }
 
-export default function run(input) {
+export default function run(input: string) {
   const lines = [...input.replace(/\\r/g, '').trim().split('\\n')];
   part1(lines);
   part2(lines);
 }
 `;
 
-fs.writeFileSync(`${dir}/index.js`, code.trim());
+await Deno.writeTextFileSync(`${dir}/index.ts`, code.trim());
 
 console.log('Setup complete.');
