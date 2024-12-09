cd1d05a1e2a961bac4bba946665dcfcf659567ef
Christian Leingang <cleingang@pm.me>
2024-12-09 16:48:33 +0100

    fix debug bug

--- utils/run.ts
+++ utils/run.ts
@@ -1,13 +1,14 @@
-import { resolve } from 'https://deno.land/std@0.224.0/path/mod.ts';
+import { resolve } from "https://deno.land/std@0.224.0/path/mod.ts";
 
 export function getInput(dayPath: string, isTest: boolean): string {
-  const fileName = isTest ? 'input_test.txt' : 'input.txt';
+  const fileName = isTest ? "input_test.txt" : "input.txt";
   return Deno.readTextFileSync(resolve(dayPath, fileName));
 }
 
-const isTest: boolean = Deno.args[0] === 'test';
-const parameter: number = isTest ? 3 : 2;
-const day: string = Deno.args[parameter] || new Date().getDate().toString().padStart(2, '0');
+const isTest: boolean = Deno.args[0] === "test";
+const parameter: number = isTest ? 1 : 0;
+const day: string = Deno.args[parameter] ||
+  new Date().getDate().toString().padStart(2, "0");
 const input: string = getInput(`./day${day}/`, isTest);
 
 import(`../day${day}/index.ts`).then((module) => {
