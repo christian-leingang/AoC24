d007b90badd5ea53d9218cc0822ba220b119dab9
Christian Leingang <cleingang@pm.me>
2024-12-18 19:28:34 +0100

    finish day11 part2

--- day11/index.ts
+++ day11/index.ts
@@ -29,7 +29,34 @@ function part1(input: number[]) {
 }
 
 function part2(input: number[]) {
+  let beforeStones = new Map(input.map((num) => [num, 1]));
+
   let result = 0;
+
+  const afterStones = new Map(beforeStones);
+
+  for (let blink = 0; blink < 75; blink++) {
+    for (const [key, val] of beforeStones) {
+      if (key === 0) {
+        afterStones.set(1, (afterStones.get(1) ?? 0) + val);
+      } else if (key.toString().length % 2 === 0) {
+        const middle = key.toString().length / 2;
+        const firstHalf = +key.toString().substring(0, middle);
+        const secondHalf = +key.toString().substring(
+          middle,
+          key.toString().length,
+        );
+        afterStones.set(firstHalf, (afterStones.get(firstHalf) ?? 0) + val);
+        afterStones.set(secondHalf, (afterStones.get(secondHalf) ?? 0) + val);
+      } else {
+        afterStones.set(key * 2024, (afterStones.get(key * 2024) ?? 0) + val);
+      }
+      afterStones.set(key, (afterStones.get(key) ?? 0) - val);
+    }
+    beforeStones = new Map(afterStones);
+  }
+  afterStones.forEach((val) => result += val);
+
   console.log("Part 2: ", result);
 }
 
